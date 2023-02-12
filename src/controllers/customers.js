d02cba23da627f75001ecf5c4e74eb92018a0c2a
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-12 15:41:51 -0300

    cadastration clients + controllers && criptografy of passwords

---
+++ src/controllers/customers.js
@@ -0,0 +1,28 @@
+const CustomersModel = require('../models/customers')
+const { crypto } = require('../utils/password')
+
+async function add(req, res) {
+  //dados que estão vindo do form
+  const {
+    name,
+    age,
+    email,
+    password,
+  } = req.body
+
+  const passwordCrypto = await crypto(password)
+
+  const register = new CustomersModel({
+    name,
+    age,
+    email,
+    password: passwordCrypto,
+  })
+
+  register.save()
+  res.send('Cadastro realizado')
+}
+
+module.exports = {
+  add
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 const router = require('express').Router()
 
+const CustomersController = require('../controllers/customers')
+
 //rotas
 router.get('/', (req, res) => {
   res.render('index', {
     title: 'Titulo Teste'
   })
 })
+
+router.get('/register', (req, res) => {
+  res.render('register', {
+    title: 'Cadastro de Clientes'
+  })
+})
+
+router.post('/register/add', CustomersController.add)
 
 module.exports = router
