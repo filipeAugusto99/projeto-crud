49036fb61d11dc54b54fd61006974e6c7fe43e16
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-12 13:43:31 -0300

    defining structure of rotes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const db = require('./database')
+const routes = require('./routes')
 
 const app = express()
 
@@ -17,12 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 //habilita o server para receber dados via post(form)
 app.use(express.urlencoded({ extended: true }))
 
-//rotas
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Titulo Teste'
-  })
-})
+//definindo as rotas
+app.use('/', routes)
 
 //404 error (not found)
 
