bd641bdec52f5bcfdd735ae74ddb46381f655af6
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-13 13:56:43 -0300

    listing clients

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,6 +1,15 @@
 const CustomersModel = require('../models/customers')
 const { crypto } = require('../utils/password')
 
+const defaultTitle = 'Cadastro de Clientes'
+
+function index(req, res) {
+  res.render('register', {
+    title: defaultTitle
+  })
+}
+
+
 async function add(req, res) {
   //dados que estão vindo do form
   const {
@@ -20,9 +29,24 @@ async function add(req, res) {
   })
 
   register.save()
-  res.send('Cadastro realizado')
+  res.render('register', {
+    title: defaultTitle,
+    message: 'Cadastro realizado com sucesso!'
+  })
+}
+
+async function listUsers(req, res) {
+  //metodo do mongoose para procurar usuarios "find"
+  const users = await CustomersModel.find()
+
+  res.render('listUsers', {
+    title: "Listagem de Usuários",
+    users
+  })
 }
 
 module.exports = {
-  add
+  index,
+  add,
+  listUsers,
 }
